b0b06c12d8853ccbddbf7e7546bdb15fb833eb40
liulin.wonder <liulin.wonder@bytedance.com>
2021-01-14 18:03:36 +0800

    feat: imooc learning

--- array.go
+++ array.go
@@ -49,22 +49,30 @@ func main()  {
 	for i:=range dd{
 		fmt.Println("键",i)
 	}
-	// 切片是数组的抽象。
-	//切片使用数组作为底层结构。
-	//切片包含三个组件：容量，长度和指向底层数组的指针,切片可以随时进行扩展
-//	slice1 := make([]float32, 0) // 长度为0的切片
+	// 切片是数组的抽象，与数组不同的是无法通过切片类型来确定其值的长度。
+	// 切片使用数组作为底层结构。
+	// 切片包含三个组件：容量，长度和指向底层数组的指针,切片可以随时进行扩展
+	slice1 := make([]float32, 0) // 长度为0的切片
+	// key: 设置不同容量但长度不一样的切片
 	slice2 := make([]float32, 3, 5) // [0 0 0] 长度为3容量为5的切片
 	// cap 函数可以用于计算数组的容量。
 	// 不过对于数组类型来说， len 和 cap 函数返回的结果始终 是一样的，都是对应数组类型的长度
-	fmt.Println(len(slice2), cap(slice2)) // 3 5
+	fmt.Printf("slice1 长度：[%d], 容量：[%d]", len(slice1), cap(slice1))   // 3 5
+	fmt.Println()
+	fmt.Printf("slice2 长度：[%d], 容量：[%d]\n", len(slice2), cap(slice2)) // 3 5
 	// 添加元素，切片容量可以根据需要自动扩展
 	slice2 = append(slice2, 1, 2, 3, 4) // [0, 0, 0, 1, 2, 3, 4]
 	fmt.Println(len(slice2), cap(slice2)) // 7 12
 
 	// 子切片 [start, end)
-	//sub1 := slice2[3:] // [1 2 3 4]
-	//sub2 := slice2[:3] // [0 0 0]
-	//sub3 := slice2[1:4] // [0 0 1]
-	//// 合并切片
-	//combined := append(sub1, sub2...) // [1, 2, 3, 4, 0, 0, 0]
+	sub1 := slice2[3:] // [1 2 3 4]
+	sub2 := slice2[:3] // [0 0 0]
+	//sub3 := slice2[3:4]
+	// 合并切片
+	combined := append(sub1, sub2...)
+	combined1 := append([]int{1,2,3}, []int{1,2,3}...)
+	fmt.Printf("combined:%v", combined)
+	fmt.Println()
+	fmt.Printf("combined1:%v", combined1)
+
 }
---
+++ slice_imooc_prac.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+
+func main() {
+	var numbers4 = [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// 获取的是索引 [4,6) 的数据，同时最大索引是 8
+	slice5 := numbers4[4:6:8]
+	length := 2
+	capacity := 4
+	fmt.Printf("%v, %v, %v\n", slice5,length == len(slice5), capacity == cap(slice5))
+	// key: 因为容量拉大，而切片实际上是引用类型，此时是 5,6,7,8
+	slice5 = slice5[:cap(slice5)]
+	slice5 = append(slice5, 11, 12, 13)
+	length = 7
+	fmt.Printf("%v\n", length == len(slice5))
+	// key: 此时 slice5 = [0,0,0,8,11,12,13]
+	slice6 := []int{0, 0, 0}
+	copy(slice5, slice6)
+	e2 := 0
+	e3 := 8
+	e4 := 11
+	fmt.Printf("%v, %v, %v\n", e2 == slice5[2], e3 == slice5[3], e4 == slice5[4])
+}
