5a4102aaf0ab842e0e6887f50efbfff771a0bfd8
liulin.wonder <liulin.wonder@bytedance.com>
2022-06-10 14:45:43 +0800

    feat: add slice learn

--- slice.go
+++ slice.go
@@ -42,6 +42,48 @@ func main() {
 	p := &s[2] // *int, tip 获取底层数组元素指针。
 	*p += 100 // 对指针所指的位置的值增加 100
 	// TODO 了解指针
-
 	fmt.Println("===s 变化后===",s)
+
+
+	// append 操作切片，向 slice 尾部添加数据，返回新的 slice 对象
+	// tip 当 append 超出了原 slice.cap 限制，就会重新分配底层数组，即使原数组没有被填满
+	var a = []int {1,2,3}
+	var b = []int{4,5,6}
+	c := append(a,b...)
+	fmt.Printf("slice c: %v\n", c)
+	d :=append(c,7)
+	fmt.Printf("slice c,d: %v, %v\n", c, d)
+
+
+	// 切片拷贝
+	data := [...]int{0,1,2,3,4,5,6,7,8,9}
+	ss1 := data[8:] // [8,9]
+	ss2 := data[:5] // [0,1,2,3,4]
+	copy(ss2,ss1)
+	fmt.Println("====>ss1", ss1)
+	fmt.Println("====>ss2", ss2)
+
+
+	// slice 遍历
+	for index,val := range ss1 {
+		fmt.Println("====>ss1 ele",index ,val)
+	}
+
+	// slice resize
+	var aa = []int{1, 3, 4, 5}
+	fmt.Printf("slice aa : %v , len(aa) : %v\n", aa, len(aa))
+	bb := aa[1:2]
+	fmt.Printf("slice bb : %v , len(bb) : %v\n", bb, len(bb))
+	cc := bb[0:3]
+	fmt.Printf("slice cc : %v , len(cc) : %v\n", cc, len(cc))
+
+	// 字符串和切片 string 底层是一个 byte 的数组
+	strr := "hello world"
+	// 含有中文字符的字符串则使用 []rune(strr)
+	ss := []byte(strr)
+	ss[6] = 'Z'
+	ss = append(ss, '!')
+	fmt.Println("=====>变化后的字符串", string(ss))
+
+
 }
