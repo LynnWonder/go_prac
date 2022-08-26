b3646edeb9e3f25b3d926aa84918060a9ce4ff62
liulin.wonder <liulin.wonder@bytedance.com>
2022-08-26 12:12:40 +0800

    feat: add pointer usage

--- pointer_value.go
+++ pointer_value.go
@@ -8,13 +8,16 @@ type B struct {
 	thing int
 }
 
+type C struct {
+	thinga *string
+	thingb *string
+}
+
 // change 方法接受一个指向 B 的指针并改变它的内部成员
 func (b *B) change() { b.thing = 1 }
 
 // write 方法通过拷贝接受 B 的值并输出 B 的内容
 func (b B) write() string { return fmt.Sprint(b) }
-
-
 
 type List []int
 
@@ -25,6 +28,7 @@ func (l List) Len() int {
 func (l *List) Append(val int) {
 	*l = append(*l, val)
 }
+
 // tip
 //  指针方法和值方法都可以在指针或者非指针上调用
 func main() {
@@ -37,7 +41,6 @@ func main() {
 	b2.change()
 	fmt.Println(b2.write())
 
-
 	// 值
 	var lst List
 	lst.Append(1)
@@ -49,10 +52,16 @@ func main() {
 	plst.Append(2)
 	// tip 当调用 Len() 方法时指针会自动解引用，变成 (*plst).Len()
 	fmt.Printf("输出 plst 相关内容===> %v (len: %d)\n", plst, plst.Len())
+
+	// TIP 使用指针作为值，可以避免结构体默认赋初值，从而判断哪些参数传入，哪些没传
+	str := "string"
+	test := C{
+		thinga: &str,
+	}
+	fmt.Println(test.thinga, test.thingb)
 }
 
 /* 输出：
 {1}
 {1}
 */
-
