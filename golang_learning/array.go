741ff960af916ea5bf49919cbff2b5b214fc127e
liulin.wonder <liulin.wonder@bytedance.com>
2022-12-01 16:43:12 +0800

    feat: add reflect

--- golang_learning/array.go
+++ golang_learning/array.go
@@ -5,7 +5,6 @@ import (
 	"reflect"
 )
 
-
 func printArr(arr *[5]int) {
 	arr[0] = 10
 	for i, v := range arr {
@@ -22,17 +21,15 @@ func sumArr(a [5]int) int {
 	return sum
 }
 
-
-func main()  {
+func main() {
 	// tip 数组定义时都是有长度定义的，比如 [len]int{} 或者 [...]int{}
 	// 	  实际开发中用到数组的概率不大
 	//var arr [6]int
 	// 声明时初始化
-	var arr = [5]int{1,2,3,4,5}
-	arr1 :=[2]int{1,2}
-	fmt.Println("===arr 的类型===",reflect.TypeOf(arr))
+	var arr = [5]int{1, 2, 3, 4, 5}
+	arr1 := [2]int{1, 2}
+	fmt.Println("===arr 的类型===", reflect.TypeOf(arr), reflect.TypeOf(arr).Kind())
 	fmt.Println(arr1)
-
 
 	var arr2 [5]int
 	printArr(&arr2)
@@ -42,56 +39,52 @@ func main()  {
 	fmt.Println(arr3)
 
 	// 修改数组
-	for i :=0; i < len(arr); i++ {
+	for i := 0; i < len(arr); i++ {
 		arr[i] += 100
 	}
 	fmt.Println(arr)
 
-
 	// tip 切片是数组的抽象。 切片使用数组作为底层结构。切片包含三个组件：容量，长度和指向底层数组的指针,切片可以随时进行扩展
 	// 	切片就是更加灵活的数组，也可以理解为就是动态数组
 	// 长度为 3 容量为 5 的切片
-	slice1 :=make([]float32, 3, 5)
+	slice1 := make([]float32, 3, 5)
 	// 此时值为 [0,0,0]
 	fmt.Println(slice1)
-
 
 	// 数组拼接，数组切割（获取数组的一部分）
 	s1 := arr[2:5:5]
 	fmt.Println(s1, len(s1), cap(s1), reflect.TypeOf(s1), reflect.TypeOf(arr))
-	for i:=0;i<len(s1);i++ {
-		s1[i] -=100
+	for i := 0; i < len(s1); i++ {
+		s1[i] -= 100
 	}
 	// 由此可见更改了数组切割下来的数据后原数组也会发生变化
-	fmt.Println("切片是引用类型，但是数组是值类型====>",s1, len(s1), cap(s1), reflect.TypeOf(s1), arr, reflect.TypeOf(arr))
+	fmt.Println("切片是引用类型，但是数组是值类型====>", s1, len(s1), cap(s1), reflect.TypeOf(s1), arr, reflect.TypeOf(arr))
 	// 切片拼接
-	var a = []int{1,2,3}
+	var a = []int{1, 2, 3}
 	a = append(a, []int{4}...)
 	a = append([]int{0}, a...)
 	b := a[:len(a)-1]
-	for i :=range b {
-		b[i] +=10
+	for i := range b {
+		b[i] += 10
 	}
 	// 可以发现给 b 赋值 a 切掉最后一个元素后的数组，a 并没有变
 	// 但是修改 b 之后 a 还是发生了变化
-	fmt.Println("切片截取",a, b)
+	fmt.Println("切片截取", a, b)
 	// 可以使用 copy 来做切片的各种变化
-
-
 
 	// 数组遍历
 	// 使用 for range 迭代的性能更好一些，这种迭代保证不会出现数据越界的情形，因此每轮迭代也就省去了对下标是否越界的判断
 	// 打印键，即下标
-	for i :=range arr {
+	for i := range arr {
 		// 整型输出
 		fmt.Printf("arr[%d]: %d\n", i, arr[i])
 	}
 	// 打印键值
-	for i,v :=range arr {
+	for i, v := range arr {
 		fmt.Printf("arr[%d]: %d\n", i, v)
 	}
 
-	for i:=0;i<len(arr);i++ {
+	for i := 0; i < len(arr); i++ {
 		fmt.Printf("arr[%d]: %d\n", i, arr[i])
 	}
 	fmt.Printf("sum=%d\n", sumArr(arr))
--- reflect.go
+++ reflect.go
@@ -11,6 +11,10 @@ type People struct {
 }
 
 func main() {
+	/*
+		tip: reflect 实现了运行时反射，允许程序操作任意类型的数据，经常使用的场景是使用 reflect.TypeOf() 来获取数据类型
+			reflect.ValueOf() 则是返回其运行时的值，indirect 返回指针类型的值指向的值
+	*/
 	content := "Name"
 	peo := new(People)
 	//Elem()获取指针对应元素的值
@@ -19,8 +23,8 @@ func main() {
 	fmt.Println(v.FieldByName(content).CanSet())
 
 	//需要修改属性的内容时，要求结构体中属性名首字母大写才可以设置
-	v.FieldByName(content).SetString("minger")
-	v.FieldByName("Address").SetString("shenzhen")
+	v.FieldByName(content).SetString("test")
+	v.FieldByName("Address").SetString("Beijing")
 	peo1 := &People{
 		"lynn",
 		"aa@163.com",
@@ -28,7 +32,24 @@ func main() {
 	sl := []*People{peo, peo1}
 	val := reflect.ValueOf(sl)
 	fmt.Println(peo)
-	fmt.Println("reflect 获取值===>", val.Index(0).Interface())
-	kind := val.Kind()
-	fmt.Println("reflect 获取值的类型===>", kind == reflect.Slice)
+	fmt.Println("reflect 获取值===>", val, sl, val.Index(0).Interface())
+	// indirect 将获取指针指向的值，如果不是指针类型就不要回做额外的操作
+	indirectVal := reflect.Indirect(reflect.ValueOf(sl))
+	fmt.Println("reflect.indirect 获取值===>", indirectVal.Index(0).Interface())
+
+	peo1Val := reflect.ValueOf(peo1)
+	peo1IndirectVal := reflect.Indirect(reflect.ValueOf(peo1))
+	fmt.Println("reflect 获取值 peo1Val===>", peo1Val.Interface())
+	// indirect 这里获取的是指针指向的值
+	fmt.Println("reflect.indirect 获取值===>", peo1IndirectVal.Interface())
+
+	mm := map[string]string{"a": "aVal", "b": "bVal", "c": "cVal"}
+	mmVal := reflect.ValueOf(mm)
+	fmt.Println("reflect 获取  mmVal 值的类型===>", mmVal.Kind())
+	mapRange := mmVal.MapRange()
+	for mapRange.Next() {
+		fmt.Println("map 中的每一个键值对===>", mapRange.Key().Interface(), mapRange.Value().Interface())
+	}
+	//kind := val.Kind()
+	//fmt.Println("reflect 获取值的类型===>", kind == reflect.Slice)
 }
